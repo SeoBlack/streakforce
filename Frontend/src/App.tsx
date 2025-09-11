6d92af0b01c3f7d2aa01a0fd5c1298098fd52b85
SeoBlack <sooreennadum2001@gmail.com>
2025-09-11 17:02:00 +0300

    add typescript and create folder structure

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <>
       <div className="text-2xl justify-center items-center h-screen flex">
         <p className="text-amber-200">
-          Hello, This is the front end of our project <n />
+          Hello, This is the front end of our project <br />
           <span className="text-blue-900 font-semibold">Streak-Force</span>
         </p>
       </div>
