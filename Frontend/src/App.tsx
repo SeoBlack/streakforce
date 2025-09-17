8f8123fd9c7aaaa12e0ae300dcbeaf2808e6701e
aashish0770 <ssuzzan81@gmail.com>
2025-09-17 21:31:01 +0300

    Not found page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,6 +5,7 @@ import MainLayout from "./pages/auth-pages/MainLayout";
 import WelcomePage from "./pages/auth-pages/WellcomePage";
 import LoginPage from "./pages/auth-pages/LoginPage";
 import HomePage from "./pages/home/HomePage";
+import NotFoundPage from "./pages/NotFound";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route element={<AuthLayout />}>
             <Route index element={<WelcomePage />} />
             <Route path="login" element={<LoginPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Route>
           <Route element={<MainLayout />}>
             <Route path="home" element={<HomePage />} />
