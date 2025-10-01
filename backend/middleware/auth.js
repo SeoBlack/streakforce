b7596936dc79fbd7c44169a69ff89a09ae6d1c7c
SeoBlack <sooreennadum2001@gmail.com>
2025-10-01 14:16:08 +0300

    fix backend google auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,6 +4,17 @@ const User = require("../models/User");
 const auth = async (req, res, next) => {
   try {
     //handle auth
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId);
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    req.user = user;
+    req.token = token;
     next();
   } catch (error) {
     res.status(401).json({ message: "Token is not valid" });
